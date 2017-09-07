d4484c3c819e174b7d15500579c10ab70a9f97a3
Christo Crampton <christo@creativecolibri.com>
2017-09-07 10:44:28 +0200

    Set environment variables from outside the service

--- backup.sh
+++ backup.sh
@@ -1,13 +1,6 @@
 #!/bin/bash -ex
 
-DATE=$(date +%Y-%m-%d-%H.%M.%S)
-
-PGDATABASE=${DATABASE_NAME}
-PGHOST=${DATABASE_HOST}
-
-PGUSER=${DATABASE_USER}
-PGPASSWORD=${DATABASE_PASSWORD}
-
+export DATE=$(date +%Y-%m-%d-%H.%M.%S)
 pg_dump --column-inserts --data-only | bzip2 -c > ~/postgres-backup-data-only-$DATE.sql.bz2
 s3cmd put ~/postgres-backup-data-only-$DATE.sql.bz2 s3://${BUCKET_NAME}/backups/
 rm ~/postgres-backup-data-only-$DATE.sql.bz2
