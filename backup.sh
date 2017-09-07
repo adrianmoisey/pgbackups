a33220919d49a76567af9a16ebc6287b3eb119f5
Christo Crampton <christo@creativecolibri.com>
2017-09-07 11:07:31 +0200

    Also put the db name in the backup filename

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
 export DATE=$(date +%Y-%m-%d-%H.%M.%S)
-pg_dump --column-inserts --data-only | bzip2 -c > ~/postgres-backup-data-only-$DATE.sql.bz2
-s3cmd put ~/postgres-backup-data-only-$DATE.sql.bz2 s3://${BUCKET_NAME}/backups/
-rm ~/postgres-backup-data-only-$DATE.sql.bz2
+pg_dump --column-inserts --data-only | bzip2 -c > ~/$PGDATABASE-postgres-backup-data-only-$DATE.sql.bz2
+s3cmd put ~/$PGDATABASE-postgres-backup-data-only-$DATE.sql.bz2 s3://${BUCKET_NAME}/backups/
+rm ~/$PGDATABASE-postgres-backup-data-only-$DATE.sql.bz2
