f719f1960b3feab6fac77562378fe59d3fb2656f
Christo Crampton <christo@creativecolibri.com>
2017-09-07 11:14:09 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-export DATE=$(date +%Y-%m-%d-%H.%M.%S)
+DATE=$(date +%Y-%m-%d-%H.%M.%S)
 pg_dump --column-inserts --data-only | bzip2 -c > ~/$PGDATABASE-postgres-backup-data-only-$DATE.sql.bz2
 s3cmd put ~/$PGDATABASE-postgres-backup-data-only-$DATE.sql.bz2 s3://${BUCKET_NAME}/backups/
 rm ~/$PGDATABASE-postgres-backup-data-only-$DATE.sql.bz2
