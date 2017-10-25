b7d5274654ccc9ece420efe5bb2701f0f28ecd4f
Christo Crampton <christo@creativecolibri.com>
2017-10-25 09:35:18 +0200

    Adds a schema dump as well as the db dump

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -ex
 
 DATE=$(date +%Y-%m-%d-%H.%M.%S)
-pg_dump --column-inserts --data-only | bzip2 -c > ~/$PGDATABASE-postgres-backup-data-only-$DATE.sql.bz2
-s3cmd put ~/$PGDATABASE-postgres-backup-data-only-$DATE.sql.bz2 s3://${BUCKET_NAME}/backups/
-rm ~/$PGDATABASE-postgres-backup-data-only-$DATE.sql.bz2
+pg_dump --schema-only | bzip2 -c > ~/$PGDATABASE-postgres-backup-data-only-$DATE-schema.sql.bz2
+pg_dump --column-inserts --data-only | bzip2 -c > ~/$PGDATABASE-postgres-backup-data-only-$DATE-data.sql.bz2
+s3cmd put ~/$PGDATABASE-postgres-backup-data-only-$DATE-schema.sql.bz2 s3://${BUCKET_NAME}/backups/
+s3cmd put ~/$PGDATABASE-postgres-backup-data-only-$DATE-data.sql.bz2 s3://${BUCKET_NAME}/backups/
+rm ~/$PGDATABASE-postgres-backup-data-only-$DATE-schema.sql.bz2
+rm ~/$PGDATABASE-postgres-backup-data-only-$DATE-data.sql.bz2
