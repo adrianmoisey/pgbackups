8eab8064f5bf86e22e99e978b1562421842deada
Adrian Moisey <adrian@changeover.za.net>
2017-11-24 18:23:44 +0200

    Fix backup name

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ export AWS_SECRET_ACCESS_KEY=$(cat /run/secrets/aws_secret_access_key)
 DATE=$(date +%Y-%m-%d-%H.%M.%S)
 pg_dump --schema-only | bzip2 -c > ~/$PGDATABASE-postgres-backup-data-only-$DATE-schema.sql.bz2
 pg_dump --column-inserts --data-only | bzip2 -c > ~/$PGDATABASE-postgres-backup-data-only-$DATE-data.sql.bz2
-s3cmd put ~/$PGDATABASE-postgres-backup-data-only-$DATE-schema.sql.bz2 s3://${BUCKET_NAME}/backups/
+s3cmd put ~/$PGDATABASE-postgres-backup-schema-only-$DATE-schema.sql.bz2 s3://${BUCKET_NAME}/backups/
 s3cmd put ~/$PGDATABASE-postgres-backup-data-only-$DATE-data.sql.bz2 s3://${BUCKET_NAME}/backups/
 rm ~/$PGDATABASE-postgres-backup-data-only-$DATE-schema.sql.bz2
 rm ~/$PGDATABASE-postgres-backup-data-only-$DATE-data.sql.bz2
